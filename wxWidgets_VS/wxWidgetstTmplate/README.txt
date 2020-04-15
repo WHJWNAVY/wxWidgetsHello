8115537fd446b289580b8709929affccd5b75a80
Haijun <haijun.wang@hyku.com>
2020-04-15 17:47:05 +0800

    add wxWidgetstTmplate

---
+++ wxWidgets_VS/wxWidgetstTmplate/README.txt
@@ -0,0 +1,2 @@
+Copy wxWidgetstTmplate.zip to 
+C:\Users\<UserName>\Documents\Visual Studio 2017\Templates\ProjectTemplates\
---
+++ wxWidgets_VS/wxWidgetstTmplateTest/wxHelloApp.cpp
@@ -0,0 +1,33 @@
+/***************************************************************
+ * Name:      wxHelloApp.cpp
+ * Purpose:   Code for Application Class
+ * Author:    whjwnavy (whjwnavy@163.com)
+ * Created:   2019-10-16
+ * Copyright: whjwnavy (www.wnavy.com)
+ * License:
+ **************************************************************/
+
+#include "wxHelloApp.h"
+
+ //(*AppHeaders
+#include "wxHelloMain.h"
+#include <wx/image.h>
+//*)
+
+IMPLEMENT_APP(wxHelloApp); //告诉wxWidgets要创建哪个类的实例
+//DECLARE_APP(wxHelloApp);   //为了让wxGetApp()返回wxHelloApp类的实例
+
+bool wxHelloApp::OnInit()
+{
+    //(*AppInitialize
+    bool wxsOK = true;
+    wxInitAllImageHandlers();
+    if (wxsOK)
+    {
+        wxHelloFrame *Frame = new wxHelloFrame(0);
+        Frame->Show();
+        SetTopWindow(Frame);
+    }
+    //*)
+    return wxsOK;
+}
